e30ddcdc78d0c0783d42e39e58616cffd3ee3a80
khanhub <khanh.tran@uzabase.com>
2018-08-09 03:13:08 +0900

    path load wip

--- domain/module_factory.go
+++ domain/module_factory.go
@@ -1,10 +1,15 @@
 package domain
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 )
+
+// TODO move to config
+var ignoreDirs = []string{".git", ".idea"}
 
 type ModuleFactory struct {
 }
@@ -16,13 +21,29 @@ func NewModuleFactory() *ModuleFactory {
 func (m *ModuleFactory) LoadFirstDirLevel(rootPath string, language LanguageType) (*Modules, error) {
 	modules := EmptyModuleList()
 	// TODO impl for first dir level load strategy
+	baseDirs, err := m.buildBaseDirs(rootPath)
+	if err != nil {
+		return modules, err
+	}
+
+	for _, dir := range baseDirs {
+		modulesDir, err := m.LoadFileLevel(dir, language)
+		if err != nil {
+			return modules, err
+		}
+		if len((*modulesDir)[0].SourceFiles) > 0 {
+			modules.Add((*modulesDir)[0])
+		}
+	}
+	return modules, nil
+}
+
+func (m *ModuleFactory) LoadFileLevel(rootPath string, language LanguageType) (*Modules, error) {
+	modules := EmptyModuleList()
 	module := NewModule(language)
 	modules.Add(module)
 	filepath.Walk(rootPath, func(path string, f os.FileInfo, _ error) error {
-		if f.IsDir() {
-			module = NewModule(language)
-			modules.Add(module)
-		} else {
+		if !f.IsDir() {
 			r, err := regexp.MatchString(language.Extension(), f.Name())
 			if err == nil && r {
 				module.AddSourceFile(NewSourceFile(f.Name()))
@@ -33,14 +54,28 @@ func (m *ModuleFactory) LoadFirstDirLevel(rootPath string, language LanguageType
 	return modules, nil
 }
 
-func (m *ModuleFactory) LoadFileLevel(rootPath string, language LanguageType) (*Modules, error) {
-	modules := EmptyModuleList()
-	// TODO impl for file level load strategy
-	return modules, nil
-}
-
 func (m *ModuleFactory) LoadRecursionDirLevel(rootPath string, language LanguageType) (*Modules, error) {
 	modules := EmptyModuleList()
 	// TODO impl for first dir level load strategy
 	return modules, nil
+}
+
+func (m *ModuleFactory) buildBaseDirs(rootPath string) ([]string, error) {
+	dirs, err := ioutil.ReadDir(rootPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var baseDirs []string
+	// TODO move to config
+	ignoreDirsPattern := strings.Join(ignoreDirs, "")
+
+	for _, dir := range dirs {
+		r, err := regexp.MatchString(dir.Name(), ignoreDirsPattern)
+		if err == nil && r {
+			continue
+		}
+		baseDirs = append(baseDirs, rootPath+"/"+dir.Name())
+	}
+	return baseDirs, nil
 }
--- domain/module_factory_test.go
+++ domain/module_factory_test.go
@@ -8,20 +8,26 @@ import (
 	"github.com/khanhtc1202/chio/domain"
 )
 
+var rootPath = "/Users/khanh.tran/workspace/go/src/github.com/khanhtc1202/chio"
+
 func TestModuleFactory_LoadFileLevel(t *testing.T) {
 	moduleFac := domain.NewModuleFactory()
-	modules, err := moduleFac.LoadFileLevel("../../", domain.GO)
+	modules, err := moduleFac.LoadFileLevel(rootPath, domain.GO)
 	if err != nil {
 		t.Fatal("Error on load file level test:", err)
 	}
 	if len(*modules) == 0 {
 		t.Fatal("Count false in load module file level")
 	}
+	for _, module := range *modules {
+		files := module.GetSourceFilesPath()
+		fmt.Println(files)
+	}
 }
 
 func TestModuleFactory_LoadFirstDirLevel(t *testing.T) {
 	moduleFac := domain.NewModuleFactory()
-	modules, err := moduleFac.LoadFirstDirLevel("/Users/khanh.tran/workspace/go/src/github.com/khanhtc1202/chio", domain.GO)
+	modules, err := moduleFac.LoadFirstDirLevel(rootPath, domain.GO)
 	if err != nil {
 		t.Fatal("Error on load file by first dir level test:", err)
 	}
@@ -36,7 +42,7 @@ func TestModuleFactory_LoadFirstDirLevel(t *testing.T) {
 
 func TestModuleFactory_LoadRecursionDirLevel(t *testing.T) {
 	moduleFac := domain.NewModuleFactory()
-	modules, err := moduleFac.LoadRecursionDirLevel("../../", domain.GO)
+	modules, err := moduleFac.LoadRecursionDirLevel(rootPath, domain.GO)
 	if err != nil {
 		t.Fatal("Error on load file recursion dir level test:", err)
 	}
